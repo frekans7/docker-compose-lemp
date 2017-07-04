c1eb7a29350a755110fd29ef1d8750ecea3012b9
Serkan <serkan7@protonmail.com>
2017-07-04 22:52:33 +0300

    Update index.php

--- LEMP/code/index.php
+++ LEMP/code/index.php
@@ -1,8 +1,8 @@
 <?php
 echo '<div>
-			<a href="index.html">## Docker Containers Info</a></br>
-			<a href="http://localhost:8183/">## phpMyAdmin</a></br>
-	  </div>';
+	<a href="index.html">## Docker Containers Info</a></br>
+	<a href="http://localhost:8183/">## phpMyAdmin</a></br>
+     </div>';
 
 echo phpinfo();
 
